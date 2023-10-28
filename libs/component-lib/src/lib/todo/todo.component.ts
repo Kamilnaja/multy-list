c2df4f962f33341128d059d26564ce04e32af0a1
KAMIL.NAJA <KAMIL.NAJA@EXTERNAL.NEXIGROUP.COM>
2023-10-28 13:49:58 +0200

    adding material

--- libs/component-lib/src/lib/todo/todo.component.ts
+++ libs/component-lib/src/lib/todo/todo.component.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'shared-multy-todo',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.scss'],
 })
